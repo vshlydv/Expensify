2e06eaa59ae0551dee2cee1859e2b3b70c2d02f1
vishal10ya <yadav.vishal.in@gmail.com>
2020-07-25 20:20:42 +0530

    Updating create date and amount formatting

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -45,7 +45,7 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ error: ""}));
             this.props.onSubmit({
                 description: this.state.description,
-                amount: parseFloat(this.state.amount) * 100,
+                amount: parseFloat(this.state.amount),
                 createdAt: this.state.createdAt,
                 note: this.state.note
             });
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,39 @@
 import React from "react";
 import moment from "moment";
+import numeral from "numeral";
 import { Link } from "react-router-dom";
 
+
+// load a locale
+numeral.register('locale', 'fr', {
+    delimiters: {
+        thousands: ',',
+        decimal: '.'
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: ''
+    }
+});
+
+// switch between locales
+numeral.locale('fr');
 
 const ExpenseListItem =  ({ id, description, amount, createdAt, dispatch }) => (
     <li>
         <h3><Link to = {`/edit/${id}`}>{description}</Link></h3>
-        <p>{amount} - {console.log(createdAt.toString())}</p>
+        <p>Rs{numeral(amount).format("0,0.00")}/-
+            :
+            {moment(createdAt).format(" DD/MM/YYYY")}
+        </p>
     </li>
 )
 
